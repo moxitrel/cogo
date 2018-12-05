495c86738524adf79755962220f8b240f3947b1d
M2 <m2@M2deMacBook-Air.local>
2018-12-05 15:29:51 +0800

    Refine coroutine

--- src/coroutine/co_switch_goto_example.c
+++ src/coroutine/co_switch_goto_example.c
@@ -5,13 +5,13 @@
 // Used as generator
 //
 
-// define the argument type of generate_n()
+// define coroutine generate_n()
 typedef struct {
     // co_t must be inherited (as first field) by user-defined type.
     co_t co;
 
     // user definitions
-    uint i;   // current number, also as return value
+    uint i;   // current generated number
     uint max; // stop when i == max
 } generate_n_t;
 
@@ -19,10 +19,10 @@ typedef struct {
 void generate_n(generate_n_t *co)
 {
     // 协程开始
-    co_begin(co, 26); // 26: co_return()所在的行号(__LINE__)
+    co_begin(co, 26);
 
     // return i, i+1, ..., max
-    for (; co->i < co->max; co->i++) {
+    for (; co->i <= co->max; co->i++) {
         co_return(co);
     }
 
@@ -31,25 +31,22 @@ void generate_n(generate_n_t *co)
 }
 
 // print 1,...,9 to stdout
-void print_1_9(void)
+int main(void)
 {
     // init coroutine
-    generate_n_t *co = &(generate_n_t){
+    generate_n_t co = (generate_n_t){
         .i   = 1, // start from 1
         .max = 9, // stop  at   9
     };
 
-    // run until finished
-    // co_state() < 0 when finished
-    while (co_state(co) >= 0) {
-        generate_n(co);
-        printf("%d\n", co->i);
+    // run co until finished, when co_state() < 0
+    for (;;) {
+        generate_n(&co);
+        if (co_state(&co) < 0) { // finished?
+            break;
+        }
+        printf("gen: %d\n", co.i);
     }
-}
-
-int main(void)
-{
-    print_1_9();
 }
 
 // clang -std=c11 co_switch_goto_example.c -o /tmp/x && /tmp/x
