6636c6ef88b385ce969d6fe49a72f54c1a2c7920
M2 <m2@M2deMacBook-Air.local>
2018-12-24 20:04:10 +0800

    Refine comments

--- src/coroutine/coroutine_example.cpp
+++ src/coroutine/coroutine_example.cpp
@@ -3,12 +3,12 @@
 
 
 class CoroutineExample : public co_t {
+    //
     // a coroutine print 0x7ffee80:0, 0x7ffee80:1, ... 0x7ffee80:6
-    class Print: public co_t {  // 2. inherent co_t
-        // local variables
-        int i;
+    //
+    class Print: public co_t {  // 2. inherit co_t
+        int i;  // local variables
         
-        // coroutine function
         void operator()()       // 3. override operator(), which has the type "void ()"
         {
             co_begin(20);       // 4. coroutine begin. 20: the line number of co_return(), co_call(), co_sched()
@@ -29,10 +29,11 @@ class CoroutineExample : public co_t {
     // run coroutine1, coroutine2 concurrently
     void operator()()
     {
-        co_begin(34, 35);
+        co_begin(35, 36);
+     // co_begin();             // you can omit line numbers if enable gnuc extension
 
-        co_sched(coroutine1);   // schedule to run coroutine1 concurrently
-        co_sched(coroutine2);   // schedule to run coroutine2 concurrently
+        co_sched(coroutine1);   // run coroutine1 concurrently
+        co_sched(coroutine2);   // run coroutine2 concurrently
 
         co_end();
     }
