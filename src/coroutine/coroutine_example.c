843c0a863519da7a640bfbe63ae322b6e668bb80
M2 <m2@M2deMacBook-Air.local>
2018-12-24 19:51:41 +0800

    Add 'Getting Started' section

---
+++ src/coroutine/coroutine_example.c
@@ -0,0 +1,66 @@
+#include "coroutine.h"  // 1. include coroutine.h
+#include <stdio.h>
+
+//
+// a coroutine print 0x7ffee80:0, 0x7ffee80:1, ... 0x7ffee80:6
+//
+typedef struct {
+    co_t co;            // 2. inherent co_t (as first field)
+    int i;              // as local variable
+} co_print_t;
+
+void co_print(co_print_t *co)   // 3. define coroutine function which has the type "void (co_t *)"
+{
+    co_begin(co, 20);   // 4. coroutine begin. 17: line number of co_return(), co_call(), co_sched()
+ // co_begin(co);       //    you can omit line numbers if enable gnuc extension
+
+ // int i;              // local variable isn't allowed, define as struct field instead
+    for (co->i = 0; co->i < 7; co->i++) {
+        printf("%p:%d\n", co, co->i);
+        co_return(co);  // 5. yield
+    }
+
+    co_end(co);         // 4. coroutine end
+}
+// co_print_t constructor
+#define CO_PRINT() ((co_print_t){.co = CO(co_print),})
+
+
+typedef struct {
+    co_t co;    // inherent co_t
+    co_print_t coroutine1;
+    co_print_t coroutine2;
+} coroutine_example_t;
+
+void coroutine_example(coroutine_example_t *co) // coroutine function
+{
+    co_begin(co,39,40);
+
+    co_sched(co, &co->coroutine1);  // schedule to run coroutine1 concurrently
+    co_sched(co, &co->coroutine2);  // schedule to run coroutine2 concurrently
+
+    co_end(co);
+}
+//
+#define COROUTINE_EXAMPLE() ((coroutine_example_t){ \
+    .co = CO(coroutine_example),                    \
+    .coroutine1 = CO_PRINT(),                       \
+    .coroutine2 = CO_PRINT(),                       \
+})
+
+int main()
+{
+    // Run until finish all coroutines.
+    // Print:
+    //  0x8f0:0
+    //  0x8f0:1
+    //  0x918:0
+    //  0x8f0:2
+    //  0x918:1
+    //  0x8f0:3
+    //  ...
+    coroutine_example_t co = COROUTINE_EXAMPLE();
+    co_run((co_t *)&co);
+}
+
+// clang -std=c17 coroutine_example.c -o /tmp/coroutine_example && /tmp/coroutine_example
---
+++ src/coroutine/coroutine_example.cpp
@@ -0,0 +1,55 @@
+#include "coroutine.h"          // 1. include coroutine.h
+#include <stdio.h>
+
+
+class CoroutineExample : public co_t {
+    // a coroutine print 0x7ffee80:0, 0x7ffee80:1, ... 0x7ffee80:6
+    class Print: public co_t {  // 2. inherent co_t
+        // local variables
+        int i;
+        
+        // coroutine function
+        void operator()()       // 3. override operator(), which has the type "void ()"
+        {
+            co_begin(20);       // 4. coroutine begin. 20: the line number of co_return(), co_call(), co_sched()
+         // co_begin();         //    you can omit line numbers if enable gnuc extension
+
+         // int i;              // local variable isn't allowed (compile fail). define as member variable instead
+            for (i = 0; i < 7; i++) {
+                printf("%p:%d\n", this, i);
+                co_return();    // 5. yield
+            }
+
+            co_end();           // 4. coroutine end
+        }
+    };
+    Print coroutine1;
+    Print coroutine2;
+
+    // run coroutine1, coroutine2 concurrently
+    void operator()()
+    {
+        co_begin(34, 35);
+
+        co_sched(coroutine1);   // schedule to run coroutine1 concurrently
+        co_sched(coroutine2);   // schedule to run coroutine2 concurrently
+
+        co_end();
+    }
+};
+
+int main()
+{
+    // Run until finish all coroutines.
+    // Print:
+    //  0x8f0:0
+    //  0x8f0:1
+    //  0x918:0
+    //  0x8f0:2
+    //  0x918:1
+    //  0x8f0:3
+    //  ...
+    CoroutineExample().run();
+}
+
+// clang -std=c++17 coroutine_example.cpp -lstdc++ -o /tmp/CoroutineExample && /tmp/CoroutineExample
