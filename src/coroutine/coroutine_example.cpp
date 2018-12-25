2dbb3cd5a123f01f8646a9b7fcb647e0053a44b6
M2 <m2@M2deMacBook-Air.local>
2018-12-25 11:11:10 +0800

    Improve handling invalid pc

--- src/coroutine/coroutine_example.cpp
+++ src/coroutine/coroutine_example.cpp
@@ -1,36 +1,35 @@
-#include "coroutine.h"          // 1. include coroutine.h
+#include "coroutine.h"      // 1. include coroutine.h
 #include <stdio.h>
 
+// a coroutine print 0x7ffee80:0, 0x7ffee80:1, ... 0x7ffee80:6
+class Print: public co_t {  // 2. inherit co_t
+    // define local variables for coroutine function
+    int i;
 
-class CoroutineExample : public co_t {
-    //
-    // a coroutine print 0x7ffee80:0, 0x7ffee80:1, ... 0x7ffee80:6
-    //
-    class Print: public co_t {  // 2. inherit co_t
-        int i;  // local variables
-        
-        void operator()()       // 3. override operator(), which has the type "void ()"
-        {
-            co_begin(20);       // 4. coroutine begin. 20: the line number of co_return(), co_call(), co_sched()
-         // co_begin();         //    you can omit line numbers if enable gnuc extension
+    void operator()()       // 3. override operator(), which has the type "void ()"
+    {
+        co_begin(16);       // 4. coroutine begin, 20: the line number of co_return(), co_call(), co_sched()
+     // co_begin();         //    you can omit line numbers if enable gnuc extension
 
-         // int i;              // local variable isn't allowed (compile fail). define as member variable instead
-            for (i = 0; i < 7; i++) {
-                printf("%p:%d\n", this, i);
-                co_return();    // 5. yield
-            }
-
-            co_end();           // 4. coroutine end
+        for (i = 0; i < 7; i++) {
+            printf("%p:%d\n", this, i);
+            co_return();    // 5. yield
         }
-    };
+
+        co_end();           // 4. coroutine end
+    }
+};
+
+// a coroutine run two Print coroutine concurrently
+class CoroutineExample : public co_t {  // inherit co_t
+    // define local variables
     Print coroutine1;
     Print coroutine2;
 
-    // run coroutine1, coroutine2 concurrently
-    void operator()()
+    void operator()()           // override operator()
     {
-        co_begin(35, 36);
-     // co_begin();             // you can omit line numbers if enable gnuc extension
+        co_begin(34, 35);
+     // co_begin();
 
         co_sched(coroutine1);   // run coroutine1 concurrently
         co_sched(coroutine2);   // run coroutine2 concurrently
