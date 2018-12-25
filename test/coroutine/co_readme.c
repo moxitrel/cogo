a25f788e8f17a71511fe47b63326925c934fdc9b
M2 <m2@M2deMacBook-Air.local>
2018-12-25 22:25:41 +0800

    Refine README and example

--- test/coroutine/co_readme.c
+++ test/coroutine/co_readme.c
@@ -3,8 +3,6 @@
 //
 #include "../../src/coroutine/coroutine.h"
 
-#include <stdio.h>
-
 //
 // 2. 自定义协程结构，必须继承 co_t
 //
@@ -17,60 +15,55 @@ typedef struct {
     //
     // 定义 局部变量, 返回值, ...
     //
-    int i;
-} print_nat_t;
+    int value;
+} nat_gen_t;
 
 //
 // 3. 定义协程函数, 类型必须为 void (co_t *)
 //
-void print_nat(print_nat_t *co)
+// 自然数生成器
+void nat_gen(nat_gen_t *co)
 {
     //
     // 4. 标识 协程开始 (必须)
     //
-    co_begin(co, 21,24,...);    // 21,24,...: 列出所有 co_return(), co_call(), co_sched() 所在的行号, 即 __LINE__ 的值
-    // co_begin(co);               // 若开启GNUC扩展，可省略行号
+    co_begin(co, 38);           // 38, ...: 列出所有 co_return(), co_call(), co_sched() 所在的行号, 即 __LINE__ 的值
+ // co_begin(co);               // 若开启GNUC扩展，可省略行号
 
     //
     // 5. 用户代码 (*** 不要使用局部变量, 无法被恢复; 定义到 struct字段 中 ***)
     //            (不要使用 return)
     //
-    for (co->i = 0; co->i < 9; co->i++) {
-        printf("%d\n", co->i);
+    for (co->value = 0; ; co->value++) {
         co_return(co);          // 返回，下次被调用，从此处开始执行
     }
 
-    printf("%d\n", co->i);
-    co_return(co);              // 返回，下次被调用，从此处开始执行
-
+ // co_call(co, coroutine);     // await, 调用其他协程直至完成
+ // co_sched(co, coroutine);    // 添加协程到调度器，并发运行
 
     //
     // 4. 标识 协程结束 (必须)
     //
     co_end(co);
 }
+// 6. 定义构造器, 用 CO() 初始化 co 成员
+#define NAT_GEN()   ((nat_gen_t){.co = CO(nat_gen),})
 
-//
-// 6. 使用
-//
-void example()
-{
-    // 7. 初始化 协程结构
-    print_nat_t printNat = {
-            .co = CO(print_nat),    // 初始化 co_t
-            //  .i = ???,               // 忽略 i 的初始化
-    };
-
-    // Run until finish.
-    // Print:
-    //  0
-    //  1
-    //  2
-    //  ...
-    co_run((co_t *)&printNat);
-}
-
+// 7. 使用
+#include <stdio.h>
 int main(void)
 {
-    example();
+    // 7. 初始化 协程结构
+    nat_gen_t n1 = NAT_GEN();
+
+    nat_gen(&n1);   // n1.value = 0
+    printf("n1.value = %d\n", n1.value);
+
+    nat_gen(&n1);   // n1.value = 1
+    printf("n1.value = %d\n", n1.value);
+
+    nat_gen(&n1);   // n1.value = 2
+    printf("n1.value = %d\n", n1.value);
+
+    return 0;
 }
