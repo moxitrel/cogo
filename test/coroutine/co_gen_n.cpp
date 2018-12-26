bd27915bde726f44a4655aa028771d5e26165092
M2 <m2@M2deMacBook-Air.local>
2018-12-26 13:08:29 +0800

    Add more comments

--- test/coroutine/co_gen_n.cpp
+++ test/coroutine/co_gen_n.cpp
@@ -17,10 +17,15 @@ struct Nat : public co_t {
     }
 };
 
+#include <stdio.h>
 int main()
 {
     Nat nat;
     nat(); // nat.value: 0
     nat(); // nat.value: 1
     nat(); // nat.value: 2
+
+    printf("gen_t.size: %zd\n", sizeof(gen_t));
+    printf("fun_t.size: %zd\n", sizeof(fun_t));
+    printf("co_t.size: %zd\n", sizeof(co_t));
 }
