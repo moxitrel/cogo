c06c2f20b7af8eb46769b8761d035d37a9b75fd1
M2 <m2@M2deMacBook-Air.local>
2018-12-26 09:00:15 +0800

    Simplify Readme

--- test/coroutine/co_readme.c
+++ test/coroutine/co_readme.c
@@ -1,46 +1,26 @@
-//
 // 1. 包含头文件 "coroutine.h"
-//
+#undef __GNUC__
 #include "../../src/coroutine/coroutine.h"
-
-//
 // 2. 定义协程结构，必须继承 co_t
-//
 typedef struct {
-    //
-    // 继承co_t (作为第1个字段)
-    //
-    co_t co;
+    co_t co;    // 继承co_t (作为第1个字段)
 
-    //
     // 声明协程的 局部变量, 返回值, ...
-    //
-    int value;  // 返回值
+    int value;
 } nat_gen_t;    // 自然数生成器
 
-//
 // 3. 定义协程函数, 类型必须为 void (co_t *)
-//
-// 自然数生成器
 void nat_gen(nat_gen_t *co)
 {
-    //
-    // 4. 标识 协程开始
-    //
-    co_begin(co, 37);           // 37, ...: 列出所有 co_return(), co_call(), co_sched() 所在的行号, 即 __LINE__ 的值
-    // co_begin(co);               // 若开启GNUC扩展，可省略行号
+    co_begin(co, 20);   // 4. 标识 协程开始, 并列出所有 co_return(), co_call(), co_sched() 所在的行号, 即 __LINE__ 的值
+ // co_begin(co);       // 若开启GNUC扩展，可省略行号
 
-    //
-    // 5. 用户代码 (*** 不要使用局部变量, 无法被恢复; 定义到 struct 字段中 ***)
-    //
+    // 5. 用户代码 (不要使用局部变量, 无法被恢复; 定义到 struct 中)
     for (co->value = 0; ; co->value++) {
-        co_return(co);          // 返回，下次被调用，从此处开始执行
+        co_return(co);  // 返回，下次被调用，从此处开始执行
     }
 
-    //
-    // 4. 标识 协程结束
-    //
-    co_end(co);
+    co_end(co);         // 4. 标识 协程结束
 }
 // 6. 定义构造器, 用 CO() 初始化 co_t 成员
 #define NAT_GEN()   ((nat_gen_t){.co = CO(nat_gen),})
--- test/coroutine/co_readme.cpp
+++ test/coroutine/co_readme.cpp
@@ -1,85 +1,82 @@
 //
 // 1. 包含头文件 "coroutine.h"
-//
+#undef __GNUC__
 #include "../../src/coroutine/coroutine.h"
 #include <stdio.h>
 
 //
-// 2. 定义协程类，必须继承 co_t
+// 2. Define a class that inherit co_t
 //
-// 打印 0x7ffee80:0, 0x7ffee80:1, ... 0x7ffee80:6
-class PrintN : public co_t {
+class PrintN : public co_t {    // Print 0x7ffee80:0, 0x7ffee80:1, ... 0x7ffee80:6
     //
-    // 声明协程的 局部变量, 返回值, ...
+    // Declare local variables, parameters, return values for coroutine function
     //
     int i;
 
     //
-    // 3. 重载运算符operator(), 类型必须为 void()
+    // 3. Override operator(), which has the type "void ()"
     //
     void operator()()
     {
         //
-        // 4. 标识 协程开始
+        // 4. Set coroutine begin
         //
-        co_begin(33);       // 33: 列出所有 co_return(), co_call(), co_sched() 所在的行号, 即 __LINE__ 的值
-        // co_begin();         // 若开启GNUC扩展，可省略行号
+        co_begin(32);       // 32: list line numbers of co_return(), co_call(), co_sched(), i.e. the value of __LINE__
+     // co_begin();         // you can omit line numbers if enable GNUC extension
 
         //
-        // 5. 用户代码 (*** 无法使用局部变量, 用 成员变量 替代 ***)
+        // 5. User code (no local variable allowed, use member variable instead)
         //
         for (i = 0; i < 7; i++) {
             printf("%p:%d\n", this, i);
-            co_return();    // 返回, 下次被调用，从此处开始执行
+            co_return();    // yield
         }
 
         //
-        // 4. 标识 协程结束
+        // 4. Set coroutine end
         //
         co_end();
     }
+
+    //
+    // 6. Define constructor if needed
+    //
+// public:
+    // PrintN(...)
+    // {
+    //    ...
+    // }
 };
 
-//
-// 2. 定义协程类，必须继承 co_t
-//
-// 创建 2 个并发运行的 PrintN 协程
-class CoroutineExample : public co_t {
-    //
-    // 声明协程的 局部变量, ...
-    //
-    PrintN coroutine1;
-    PrintN coroutine2;
+// Create two PrintN coroutine
+class CoroutineExample : public co_t {  // 2. Define a class that inherit co_t
+    PrintN coroutine1;          // Declare local variables of coroutine function
+    PrintN coroutine2;          //
 
-    //
-    // 3. 重载运算符operator()
-    //
-    void operator()()
+    void operator()()           // 3. Override operator()
     {
-        //
-        // 4. 协程开始
-        //
-        co_begin(68,69);
-        // co_begin();
+        co_begin(62,63);        // 4. Coroutine begin
+     // co_begin();
 
-        //
-        // 5. 用户代码
-        //
-        co_sched(coroutine1);   // 添加 coroutine1 到调度器中运行
-        co_sched(coroutine2);   // 添加 coroutine2 到调度器中运行
+                                // 5. User code
+        co_sched(coroutine1);   // add coroutine1 to scheduler
+        co_sched(coroutine2);   // add coroutine2 to scheduler
 
-        //
-        // 4. 协程结束
-        //
-        co_end();
+        co_end();               // 4. Coroutine end
     }
+
+// public:                      // 6. Define constructor if needed
+    // CoroutineExample(...)
+    // {
+    //    ...
+    // }
 };
 
-// 6. 使用
+// 7. Use it
 int main()
 {
-    // 运行直至结束
-    // 输出:
+    // Run until finish all coroutines.
+    // Output:
     //  0x8f0:0
     //  0x8f0:1
     //  0x918:0
