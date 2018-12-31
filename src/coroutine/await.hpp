6868a78d06088a282629139441a529ed0a890993
M2 <m2@M2deMacBook-Air.local>
2018-12-31 15:24:03 +0800

    Refine await_t's run()

--- src/coroutine/await.hpp
+++ src/coroutine/await.hpp
@@ -4,24 +4,25 @@
 #include "gen.hpp"
 
 // await_t: support call stack. (behave like function, slow about 10 stores when -O)
+//  .state() -> int      : return the current running state.
 //  .step () -> await_t *: keep running until yield, return the next coroutine (call stack top) to be run.
 //  .run  ()             : keep running until finish.
-class await_t : protected gen_t {
+class await_t : public gen_t {
     // the coroutine function
     virtual void operator()() = 0;
     // the next lower stack frame
     await_t *caller = nullptr;
 protected:
-    // the stack top of current call stack, one per scheduler
+    // the stack top of current call stack
     thread_local static await_t *stack_top;
 
-    // run the coroutine pointed by stack_top until yield
+    // run the coroutine pointed by stack_top until yield, and refresh the call stack top
     static void stack_step()
     {
         assert(stack_top);
 
         if (stack_top->state() < 0) {
-            // pop
+            // stack pop
             stack_top = stack_top->caller;
         } else {
             stack_top->operator()();
@@ -30,13 +31,13 @@ protected:
 
     void _await(await_t &callee)
     {
-        // push
+        // stack push
         callee.caller = this;
         stack_top = &callee;
     }
 public:
-    // run until yield, and return the next coroutine to be run. (for generator)
-    // return NULL if coroutine finished
+    // behave the same as stack_step(), provide for generator
+    // return NULL if finished
     await_t *step()
     {
         await_t *next = this;
@@ -45,14 +46,11 @@ public:
             // set caller as the new stack top, (pop)
             next = caller;
         } else {
-            // clear stack_top
-            if (stack_top != nullptr) {
-                stack_top = nullptr;
-            }
-
+            // use stack_top as a temporary storage for callee if exist
+            // require stack_top inited to NULL, and clear when exit. (to improve performance)
             operator()();
             if (stack_top != nullptr) {
-                // set callee as the new stack top
+                // set callee as the new stack top, (push)
                 next = stack_top;
                 stack_top = nullptr;
             }
@@ -70,7 +68,8 @@ public:
     }
 };
 
-thread_local await_t *await_t::stack_top;
+// step() require stack_top inited with NULL
+thread_local await_t *await_t::stack_top = nullptr;
 
 // Call another coroutine. (await)
 // await_t::co_await(await_t &co);
