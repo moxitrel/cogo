758ff8cb2bfaf357c65abe82b67710cd042606b7
x <x@xdeMacBook-Air.local>
2021-07-21 22:32:08 +0800

    Improve queue

--- co_st.h
+++ co_st.h
@@ -26,43 +26,8 @@ CO_CHAN_READ (co_chan_t*, co_msg_t*)    : receive a message from channel, the re
 #define MOXITREL_COGO_CO_IMPL_H_
 
 #include "co.h"
-#include <stdbool.h>
+#include "co_st_q.h"
 #include <stddef.h>
-#include <stdint.h>
-
-typedef struct {
-    void* head;
-    void* tail;
-} co_queue_t;
-
-#define CO_QUEUE_NEXT(Q,N)    (*(void**)((intptr_t)(Q) + (N)))
-
-static inline bool co_queue_empty(const co_queue_t* thiz)
-{
-    return thiz->head == NULL;
-}
-
-/* dequeue */
-static inline void* co_queue_pop(co_queue_t* thiz, ptrdiff_t next)
-{
-    void* node = thiz->head;
-    if (!co_queue_empty(thiz)) {
-        thiz->head = CO_QUEUE_NEXT(thiz->head, next);
-    }
-    return node;
-}
-
-/* enqueue */
-static inline void co_queue_push(co_queue_t* thiz, ptrdiff_t next, void* node)
-{
-    if (co_queue_empty(thiz)) {
-        thiz->head = node;
-    } else {
-        CO_QUEUE_NEXT(thiz->tail, next) = node;
-    }
-    thiz->tail = node;
-    CO_QUEUE_NEXT(node, next) = NULL;
-}
 
 typedef struct co       co_t;
 typedef struct co_sch   co_sch_t;
@@ -143,7 +108,7 @@ inline int cogo_chan_read(co_t* co, co_chan_t* chan, co_msg_t* msg_next)
     COGO_ASSERT(chan->size > PTRDIFF_MIN);
     COGO_ASSERT(msg_next);
 
-    ptrdiff_t chan_size = chan->size--;
+    const ptrdiff_t chan_size = chan->size--;
     if (chan_size <= 0) {
         co_queue_push(&chan->mq, offsetof(co_msg_t, next), msg_next);
         // sleep in background
@@ -151,10 +116,10 @@ inline int cogo_chan_read(co_t* co, co_chan_t* chan, co_msg_t* msg_next)
         ((cogo_co_t*)co)->sch->stack_top = NULL;                // remove from scheduler
         return 1;
     } else {
-        msg_next->next = (co_msg_t*)co_queue_pop(&chan->mq, offsetof(co_msg_t, next));
+        msg_next->next = (co_msg_t*)co_queue_pop_nonempty(&chan->mq, offsetof(co_msg_t, next));
         // wake up a writer if exists
         if (chan_size >= chan->cap) {
-            cogo_co_t* writer = (cogo_co_t*)co_queue_pop(&chan->cq, offsetof(co_t, next));
+            cogo_co_t* writer = (cogo_co_t*)co_queue_pop_nonempty(&chan->cq, offsetof(co_t, next));
             return cogo_sch_push(((cogo_co_t*)co)->sch, writer);
         }
         return 0;
@@ -170,22 +135,22 @@ do {                                                                            
 } while (0)
 inline int cogo_chan_write(co_t* co, co_chan_t* chan, co_msg_t* msg)
 {
-  //COGO_ASSERT(co);
+//  COGO_ASSERT(co);
     COGO_ASSERT(chan);
     COGO_ASSERT(chan->size < PTRDIFF_MAX);
     COGO_ASSERT(msg);
 
-    ptrdiff_t chan_size = chan->size++;
+    const ptrdiff_t chan_size = chan->size++;
     if (chan_size < 0) {
-        ((co_msg_t*)co_queue_pop(&chan->mq, offsetof(co_msg_t, next)))->next = msg;
+        ((co_msg_t*)co_queue_pop_nonempty(&chan->mq, offsetof(co_msg_t, next)))->next = msg;
         // wake up a reader
-        cogo_co_t* reader = (cogo_co_t*)co_queue_pop(&chan->cq, offsetof(co_t, next));
+        cogo_co_t* reader = (cogo_co_t*)co_queue_pop_nonempty(&chan->cq, offsetof(co_t, next));
         return cogo_sch_push(((cogo_co_t*)co)->sch, reader);
     } else {
         co_queue_push(&chan->mq, offsetof(co_msg_t, next), msg);
         if (chan_size >= chan->cap) {
             // sleep in background
-            co_queue_push(&chan->cq, offsetof(co_t, next), co);
+            co_queue_push_nonempty(&chan->cq, offsetof(co_t, next), co);
             ((cogo_co_t*)co)->sch->stack_top = NULL;
             return 1;
         }
---
+++ co_st_q.h
@@ -0,0 +1,53 @@
+#ifndef MOXITREL_COGO_CO_IMPL_Q_H_
+#define MOXITREL_COGO_CO_IMPL_Q_H_
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct {
+    void* head;
+    void* tail;
+} co_queue_t;
+
+#define CO_QUEUE_NEXT(Q,N)    (*(void**)((intptr_t)(Q) + (N)))
+
+static inline bool co_queue_empty(const co_queue_t* thiz)
+{
+    return thiz->head == NULL;
+}
+
+static inline void* co_queue_pop(co_queue_t* thiz, ptrdiff_t next)
+{
+    void* node = thiz->head;
+    if (!co_queue_empty(thiz)) {
+        thiz->head = CO_QUEUE_NEXT(thiz->head, next);
+    }
+    return node;
+}
+
+static inline void co_queue_push(co_queue_t* thiz, ptrdiff_t next, void* node)
+{
+    if (co_queue_empty(thiz)) {
+        thiz->head = node;
+    } else {
+        CO_QUEUE_NEXT(thiz->tail, next) = node;
+    }
+    thiz->tail = node;
+    CO_QUEUE_NEXT(node, next) = NULL;
+}
+
+static inline void* co_queue_pop_nonempty(co_queue_t* thiz, ptrdiff_t next)
+{
+    void* node = thiz->head;
+    thiz->head = CO_QUEUE_NEXT(thiz->head, next);
+    return node;
+}
+
+static inline void co_queue_push_nonempty(co_queue_t* thiz, ptrdiff_t next, void* node)
+{
+    CO_QUEUE_NEXT(thiz->tail, next) = node;
+    thiz->tail = node;
+    CO_QUEUE_NEXT(node, next) = NULL;
+}
+
+#endif  // MOXITREL_COGO_CO_IMPL_Q_H_
