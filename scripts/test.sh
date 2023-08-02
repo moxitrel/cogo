792108b2e31e63056ac5ac9be229f2b4e568e6e0
moxi <6856151+moxitrel@users.noreply.github.com>
2023-08-02 20:31:42 +0800

    WIP

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
+ROOT=$(dirname $0)/..
+
 if [ -n "$1" ]; then
     BUILD_DIR="$1"
 else
-    BUILD_DIR="$(dirname $0)/build"
+    BUILD_DIR="$ROOT/build"
 fi
-
-REPO_ROOT=$(dirname $0)
 
 for CC in clang gcc; do
     for YIELD_TYPE in COGO_USE_CASE COGO_USE_LABEL_VALUE; do
         export CC="${CC}"
 
-        cmake -S $REPO_ROOT -B $BUILD_DIR -D${YIELD_TYPE}=TRUE -DCMAKE_BUILD_TYPE=MinSizeRel -GNinja --fresh &&
+        cmake -S $ROOT -B $BUILD_DIR -D${YIELD_TYPE}=TRUE -DCMAKE_BUILD_TYPE=MinSizeRel -GNinja --fresh &&
         cmake --build $BUILD_DIR --clean-first -j &&
         ctest --test-dir $BUILD_DIR --output-on-failure --schedule-random -j2 &&
         cmake --install $BUILD_DIR --prefix $BUILD_DIR/install &&
