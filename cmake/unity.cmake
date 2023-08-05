e45260fcaeace1b35a31c7fccf241bf7fcac051a
Moxi Color <6856151+moxitrel@users.noreply.github.com>
2023-08-05 21:57:43 +0800

    Update

--- cmake/unity.cmake
+++ cmake/unity.cmake
@@ -3,17 +3,17 @@ include(FetchContent)
 include(FindPackageMessage)
 
 FetchContent_Declare(unity
-    GIT_REPOSITORY https://github.com/ThrowTheSwitch/Unity.git
-    GIT_TAG v2.5.2
-    GIT_SHALLOW TRUE
+  GIT_REPOSITORY https://github.com/ThrowTheSwitch/Unity.git
+  GIT_TAG v2.5.2
+  GIT_SHALLOW TRUE
 )
 FetchContent_MakeAvailable(unity)
 
 if(NOT DEFINED unity_VERSION)
-    set(unity_VERSION v2.5.2)
+  set(unity_VERSION v2.5.2)
 endif()
 
 find_package_message(unity
-    "Found unity: ${unity_VERSION}"
-    "${unity_VERSION}"
+  "Found unity: ${unity_VERSION}"
+  "${unity_VERSION}"
 )
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,21 +2,16 @@
 
 ROOT=$(dirname "$0")/..
 
-if [ -n "$1" ]; then
-    BUILD_DIR="$1"
-else
-    BUILD_DIR="$ROOT/build"
-fi
+BUILD_DIR="$ROOT/build"
+for cc in clang gcc; do
+  for YIELD_TYPE in COGO_USE_CASE COGO_USE_LABEL_VALUE; do
+    export CC="$cc"
 
-for CC in clang gcc; do
-    for YIELD_TYPE in COGO_USE_CASE COGO_USE_LABEL_VALUE; do
-        export CC="${CC}"
-
-        cmake -S $ROOT -B $BUILD_DIR -D${YIELD_TYPE}=TRUE -DCMAKE_BUILD_TYPE=MinSizeRel -GNinja --fresh &&
-        cmake --build $BUILD_DIR --clean-first -j &&
-        ctest --test-dir $BUILD_DIR --output-on-failure --schedule-random -j2 &&
-        cmake --install $BUILD_DIR --prefix $BUILD_DIR/install &&
-        env -C $BUILD_DIR cpack -G ZIP ||
-        exit $?
-    done
+    cmake -S $ROOT -B $BUILD_DIR -D${YIELD_TYPE}=TRUE -DCMAKE_BUILD_TYPE=MinSizeRel --fresh &&
+      cmake --build $BUILD_DIR --clean-first -j2 &&
+      ctest --test-dir $BUILD_DIR --output-on-failure --schedule-random -j2 &&
+      cmake --install $BUILD_DIR --prefix $BUILD_DIR/install &&
+      env -C $BUILD_DIR cpack -G ZIP ||
+      exit $?
+  done
 done
